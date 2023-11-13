d2f15aab368ba844c1487d59d6707ce787bbd1f5
Mayron <70946246+maydoc@users.noreply.github.com>
2023-11-13 15:17:33 -0300

    Comentários no script.js

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,21 @@
+// Criando constantes do menu hamburger e do menu de links
 const hamburguer = document.querySelector("#hamburguer")
 const navmenu = document.querySelector("#nav-links")
 
+// Função para mostrar conteúdo do link
 function showContent(navID) {
+        // Armazenando todos os links que não estão sendo exibidos
         var navs = document.querySelectorAll('section > div');
         navs.forEach(function (element) {
             element.style.display = 'none';
         });
-
+        
+        // Exibindo o link atual selecionado
         var navAtual = document.getElementById(navID);
         navAtual.style.display = 'block';
     }
 
+// Definindo a função por clique para cada link
 document.querySelector('nav a[href="#about"]').addEventListener('click', function () {
         showContent('about-section');
     });
@@ -27,6 +32,7 @@ document.querySelector('nav a[href="#contact"]').addEventListener('click', funct
     showContent('contact-section');
 });
 
+// Criando um evento de clique para o menu hamburger
 hamburguer.addEventListener("click", () => {
   hamburguer.classList.toggle("active")
   navmenu.classList.toggle("active")
